324fc7a921a6ff035b00867365fec86455dc1c29
sovernut <orenomail4@gmail.com>
2020-05-22 15:31:51 +0700

    [imp]:add rest_error

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sovernut/bookstore_utils-go
+
+go 1.14
---
+++ rest_errors/rest_error_test.go
@@ -0,0 +1,34 @@
+package rest_errors
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewInternalServerError(t *testing.T) {
+	err := NewInternalServerError("message error", errors.New("database error"))
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "message error", err.Message)
+	assert.EqualValues(t, "internal_server_error", err.Error)
+
+	assert.NotNil(t, err.Causes)
+	assert.EqualValues(t, 1, len(err.Causes))
+	assert.EqualValues(t, "database error", err.Causes[0])
+
+}
+
+func TestNewBadRequestError(t *testing.T) {
+
+}
+
+func TestNewNotFoundError(t *testing.T) {
+
+}
+
+func TestNewError(t *testing.T) {
+
+}
